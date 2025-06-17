88da4e1072b0c56ffc807a7d424e25545697267c
yanqi.zong <yzong@turo.com>
2025-06-17 13:09:24 -0700

    feat: add import.meta.env.VITE_ROOT_DOMAIN

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,7 +4,7 @@ import { twMerge } from 'tailwind-merge';
 export const protocol =
   process.env.NODE_ENV === 'production' ? 'https' : 'http';
 export const rootDomain =
-  process.env.NEXT_PUBLIC_ROOT_DOMAIN || 'localhost:3000';
+  import.meta.env.VITE_ROOT_DOMAIN || 'localhost:3000';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
